e92ee42e7e302f186673c9cdc5d868f51a3ac593
IZABELA BORGES YUGULIS <izabela.byugulis@senacsp.edu.br>
2024-08-07 20:28:23 -0300

    PHP e BD

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 // ip:porta, nome do bd, login e senha
 $db_host = "localhost";
-$db_port = 3310;
-$db_name = "juquinhas";
-$db_user = "IzabelaYugulis";
-$db_pwd = "exxcv5xtbh";
-
+$db_port = 3306;
+$db_name = "juquinhabd";
+$db_user = "izabela";
+$db_pwd = "senac1";
+ 
 $pdo = new PDO("mysql:host=".$db_host.";port=".$db_port.";dbname=".$db_name, $db_user, $db_pwd);
---
+++ app/index.php
@@ -0,0 +1,3 @@
+<?php
+require "./config/config.php";
+var_dump($pdo);
